a7eb996113dd8494c641accdb0a5c19970344d04
Ravin Bhakta <ravin.bhakta@gmail.com>
2026-02-02 20:51:12 -0800

    Update redisClient.js

--- server/redisClient.js
+++ server/redisClient.js
@@ -2,7 +2,7 @@ import { createClient } from 'redis';
 import { config } from 'dotenv';
 config({ path: './.env' });
 const redisClient = createClient({
-  url: process.env.REDIS_API_KEY
+  url: process.env.REDIS_URL
 });
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
